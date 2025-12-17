2de4336bc87c2fd6ad74b76bd797361bdc388cbe
Christoph Zobel <zobel@klara-oppenheimer-schule.de>
2025-12-17 15:45:56 +0100

    Chanes SOSO for video

--- LED_BLINKEN_INTERN_SOS/LED_BLINKEN_INTERN_SOS.ino
+++ LED_BLINKEN_INTERN_SOS/LED_BLINKEN_INTERN_SOS.ino
@@ -3,65 +3,58 @@ const int ledPin =1;
 void setup() 
 { 
   pinMode(ledPin, OUTPUT); // Pin1 wird auf pinmode output gesetzt
-  digitalWrite(1, HIGH); // LED wird ausgeschalten, idem die Spannung angelegt wird
-  
-  /* Erklärung zu low und high:
-  Die interne LED des ESP32 leuchtet bei digitalWrite(1, LOW);, weil sie active low geschaltet ist 
-  das bedeutet, sie wird eingeschaltet, wenn der zugehörige GPIO-Pin auf LOW (0 V) gesetzt wird, da 
-  dann Strom von VCC über die LED zum Pin fließen kann.
-  */
+  digitalWrite(ledPin, HIGH); // LED wird ausgeschalten, idem die Spannung angelegt wird
 } 
 
 void loop() 
 { 
   //Start "S" (kurz)
-  digitalWrite(1, LOW); // turn LED on! 
+  digitalWrite(ledPin, LOW); // turn LED on! 
   delay(500); 
-  digitalWrite(1, HIGH); // turn LED off! 
-  delay(500); 
-  
-  digitalWrite(1, LOW); // turn LED on! 
-  delay(500); 
-  digitalWrite(1, HIGH); // turn LED off! 
-  delay(500); 
-
-  digitalWrite(1, LOW); // turn LED on! 
-  delay(500); 
-  digitalWrite(1, HIGH); // turn LED off! 
-  delay(500); 
-
-
-//Start "0" (lang)
-  digitalWrite(1, LOW); // turn LED on! 
-  delay(3000); 
-  digitalWrite(1, HIGH); // turn LED off! 
+  digitalWrite(ledPin, HIGH); // turn LED off! 
   delay(500); 
   
-  digitalWrite(1, LOW); // turn LED on! 
+  digitalWrite(ledPin, LOW); // turn LED on! 
+  delay(500); 
+  digitalWrite(ledPin, HIGH); // turn LED off! 
+  delay(500); 
+
+  digitalWrite(ledPin, LOW); // turn LED on! 
+  delay(500); 
+  digitalWrite(ledPin, HIGH); // turn LED off! 
+  delay(1000); 
+
+  //Start "0" (lang)
+  digitalWrite(ledPin, LOW); // turn LED on! 
   delay(3000); 
-  digitalWrite(1, HIGH); // turn LED off! 
+  digitalWrite(ledPin, HIGH); // turn LED off! 
   delay(500); 
   
-  digitalWrite(1, LOW); // turn LED on! 
+  digitalWrite(ledPin, LOW); // turn LED on! 
   delay(3000); 
-  digitalWrite(1, HIGH); // turn LED off! 
+  digitalWrite(ledPin, HIGH); // turn LED off! 
   delay(500); 
+  
+  digitalWrite(ledPin, LOW); // turn LED on! 
+  delay(3000); 
+  digitalWrite(ledPin, HIGH); // turn LED off! 
+  delay(1000); 
 
 
   //Start "S" (kurz)
-  digitalWrite(1, LOW); // turn LED on! 
+  digitalWrite(ledPin, LOW); // turn LED on! 
   delay(500); 
-  digitalWrite(1, HIGH); // turn LED off! 
+  digitalWrite(ledPin, HIGH); // turn LED off! 
   delay(500); 
   
-  digitalWrite(1, LOW); // turn LED on! 
+  digitalWrite(ledPin, LOW); // turn LED on! 
   delay(500); 
-  digitalWrite(1, HIGH); // turn LED off! 
+  digitalWrite(ledPin, HIGH); // turn LED off! 
   delay(500); 
 
-  digitalWrite(1, LOW); // turn LED on! 
+  digitalWrite(ledPin, LOW); // turn LED on! 
   delay(500); 
-  digitalWrite(1, HIGH); // turn LED off! 
-  delay(500); 
+  digitalWrite(ledPin, HIGH); // turn LED off! 
+  delay(3000);
  } 
 
